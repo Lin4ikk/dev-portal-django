8fb56530cf45f633c8b93838cdcbbe90c5e2f60a
Lin4ikk <sokolovna08@gmail.com>
2026-06-22 18:45:01 +0300

    Исправлена ошибка дублирования профилей при регистрации

--- accounts/views.py
+++ accounts/views.py
@@ -1,29 +1,22 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UserRegisterForm
 
 class RegisterView(CreateView):
-    """Представление для регистрации нового пользователя."""
     form_class = UserRegisterForm
     template_name = 'accounts/register.html'
-    success_url = reverse_lazy('accounts:login')  # После успешной регистрации отправляем на страницу входа
+    success_url = reverse_lazy('accounts:login')
 
 class MyLoginView(LoginView):
-    """Представление для авторизации (входа)."""
     template_name = 'accounts/login.html'
     
     def get_success_url(self):
-        # После входа перенаправляем на главную страницу блога
         return reverse_lazy('blog:home')
 
 class MyLogoutView(LogoutView):
-    """Представление для выхода из системы."""
-    next_page = reverse_lazy('blog:home')  # После выхода возвращаем на главную
-
-from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin # Защита страницы от гостей
+    next_page = reverse_lazy('blog:home')
 
 class ProfileView(LoginRequiredMixin, TemplateView):
-    """Страница личного кабинета пользователя."""
     template_name = 'accounts/profile.html'
